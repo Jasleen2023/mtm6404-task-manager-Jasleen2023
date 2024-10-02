1156af6ba380021cf67508b93114581ff4a473ab
Jasleen2023 <145064057+Jasleen2023@users.noreply.github.com>
2024-10-02 12:54:12 -0400

    Delete app.test.js

--- app.test.js
+++
@@ -1,9 +0,0 @@
-// app.test.js
-import { render, screen } from '@testing-library/react';
-import App from './app';
-
-test('renders welcome message', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Welcome to Task Manager/i);
-  expect(linkElement).toBeInTheDocument();
-});
